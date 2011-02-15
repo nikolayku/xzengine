1c4198abe7c70d9b24ebbdfdbf0642960e7304e1
Nikolayku@gmail.com <Nikolayku@gmail.com@213987e7-2c6c-a716-a7a9-749caac13233>
2011-02-15 14:53:33 +0000

    -- fix editnews for users

--- classes/addnews.php
+++ classes/addnews.php
@@ -205,13 +205,21 @@ class addnews
 
 				tinyMCE.init({
 				mode : "textareas",
-				theme_advanced_toolbar_location : "top",
+				theme: "advanced",
+				language : "en",
 				plugins : "emotions",
 				theme_advanced_buttons1 : "separator,justifyleft,justifycenter,justifyfull,justifyright,separator,bold,italic,underline,strikethrough,separator,link,image,separator,emotions,separator",
 				theme_advanced_buttons2 : "",
 				theme_advanced_buttons3 : "",
+				theme_advanced_buttons4 : "",
+				theme_advanced_toolbar_location : "top",
+				theme_advanced_toolbar_align : "left",
+				theme_advanced_statusbar_location : "bottom",
+				theme_advanced_resizing : true,
+
+
 				relative_urls : false,
-				convert_urls : false
+				convert_urls : false,
 				});
 
 				</script>';	
