3caea6bb5f0578f8f0efc61dac59492747ad9090
nikolayku@gmail.com <nikolayku@gmail.com@213987e7-2c6c-a716-a7a9-749caac13233>
2011-11-26 20:28:37 +0000

    -- fix add many elements

--- skin/xzengine_ru/plugins/price/code.js
+++ skin/xzengine_ru/plugins/price/code.js
@@ -2,12 +2,15 @@ function showHidePricePlugin()
 {	
 	var main_div = document.getElementById("price_plugin_main_div");
 	
-	var el = document.createElement('iframe');
-	el.setAttribute('src', 'http://docs.google.com/viewer?embedded=true&url={price_url}');
-	el.setAttribute('width', "100%");
-	el.setAttribute('height', "100%");
-	main_div.appendChild(el);
-	
+	if(document.getElementById("google_document_viewer") == null)
+	{
+		var el = document.createElement('iframe');
+		el.setAttribute('id', 'google_document_viewer');
+		el.setAttribute('src', 'http://docs.google.com/viewer?embedded=true&url={price_url}');
+		el.setAttribute('width', "100%");
+		el.setAttribute('height', "100%");
+		main_div.appendChild(el);
+	}
 	
 	if(main_div.style.visibility == "visible")
 	{
